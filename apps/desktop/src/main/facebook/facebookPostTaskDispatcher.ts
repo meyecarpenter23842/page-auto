0f3b901078e352c08fa09645071e408146f6c001
binhnxwjfjxm <binhnxwjfjxm@hotmail.com>
2026-09-04 10:32:08 +0700

    wip: spin content before Facebook posting

--- apps/desktop/src/main/facebook/facebookPostTaskDispatcher.ts
+++ apps/desktop/src/main/facebook/facebookPostTaskDispatcher.ts
@@ -5,6 +5,7 @@ import {
   type GroupPostTaskJobRequest,
   type PageWallPostTaskJobRequest
 } from '../../shared/facebookTasks'
+import { spinContent } from '../../shared/contentSpin'
 import type { PostingJobResult } from '../../shared/posting'
 import { executePostingJob } from '../browser/posting/postingEngine'
 import { executePageWallPostJob } from '../business/page-wall-post/executePageWallPostJob'
@@ -21,15 +22,23 @@ function isPageWallPostTaskJob(job: FacebookPostTaskJobRequest): job is PageWall
   return job.task.type === 'page_wall_post'
 }
 
+function spinJobContent<T extends FacebookPostTaskJobRequest>(job: T): T {
+  return {
+    ...job,
+    content: spinContent(job.content)
+  }
+}
+
 export async function executeFacebookPostTaskJob(job: FacebookPostTaskJobRequest): Promise<PostingJobResult> {
   const validationError = validateFacebookPostTaskJob(job)
   if (validationError) return invalidTask(validationError)
 
-  if (isGroupPostTaskJob(job)) {
-    return executePostingJob(legacyPostingJobFromGroupTask(job))
+  const runtimeJob = spinJobContent(job)
+  if (isGroupPostTaskJob(runtimeJob)) {
+    return executePostingJob(legacyPostingJobFromGroupTask(runtimeJob))
   }
-  if (isPageWallPostTaskJob(job)) {
-    return executePageWallPostJob(job)
+  if (isPageWallPostTaskJob(runtimeJob)) {
+    return executePageWallPostJob(runtimeJob)
   }
 
   return invalidTask('Facebook posting worker nhận task type chưa được hỗ trợ.')
